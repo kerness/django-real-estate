387436f6e79e465383b3b557aaca60d4bf74f03e
kerness <maciej.bak260@gmail.com>
2022-09-09 23:48:42 +0200

    bug: typo

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path("supersecret/", admin.site.urls),
+    path("supersecret", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

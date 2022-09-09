f433b9346f103604cf5ee801e7697764f4e0af7f
kerness <maciej.bak260@gmail.com>
2022-09-09 23:45:36 +0200

    bug: correct db engine

--- real_estate/settings/development.py
+++ real_estate/settings/development.py
@@ -3,7 +3,7 @@ from .base import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': env('POSTGRES_DB'),
         'USER': env('POSTGRES_USER'),
         'PASSWORD': env('POSTGRES_PASS'),

e0170d53c9055cd797885b835c7d9a9a034e0eb9
kerness <maciej.bak260@gmail.com>
2022-09-11 10:57:44 +0200

    feat: user auth-register,login,pass reset

--- real_estate/settings/development.py
+++ real_estate/settings/development.py
@@ -1,13 +1,22 @@
 from .base import *
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = env("EMAIL_PORT")
+DEFAULT_FORM_EMAIL = "info@real_estate.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Real Estate"
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('POSTGRES_DB'),
-        'USER': env('POSTGRES_USER'),
-        'PASSWORD': env('POSTGRES_PASS'),
-        'HOST': env('PG_HOST'),
-        'PORT': env('PG_PORT'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASS"),
+        "HOST": env("PG_HOST"),
+        "PORT": env("PG_PORT"),
     }
 }
